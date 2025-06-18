527f62c609980a717adf2ec8f5b121c7d3a5e3d7
AgusBarre <acordobab@fi.uba.ar>
2025-06-18 20:04:43 -0300

    Resuelvo problemas y cambio nombre de archivos

--- main.c
+++ main.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-// Declaración de la función en assembler
-extern void base64_encode(const unsigned char* input, unsigned int len, char* output);
 
 int main() {
-    FILE* input_file = fopen("inputBinario.bin", "rb");
-    if (!input_file) {
-        perror("Error al abrir inputBinario.bin");
+    FILE *inputBinario;
+    FILE *outputTexto;  
+    char buffer[198];    
+    size_t cantidadLeida;
+
+    char nombreArchivoEntrada[] = "inputBinario.bin";
+    char nombreArchivoSalida[] = "outputTexto.txt";
+
+
+    inputBinario = fopen(nombreArchivoEntrada, "rb");  
+    if (inputBinario == NULL) {
+        printf("No se pudo abrir el archivo.\n");
         return 1;
     }
 
-    // Obtener tamaño del archivo
-    fseek(input_file, 0, SEEK_END);
-    long file_size = ftell(input_file);
-    fseek(input_file, 0, SEEK_SET);
+    cantidadLeida = fread(buffer, 1, 198, inputBinario);  
+    printf("Se leyeron %zu bytes.\n", cantidadLeida);
 
-    // Leer datos binarios
-    unsigned char* input_data = malloc(file_size);
-    fread(input_data, 1, file_size, input_file);
-    fclose(input_file);
+    fclose(inputBinario);
 
-    // Calcular tamaño de salida (4*(n/3) + padding)
-    long output_size = 4 * ((file_size + 2) / 3);
-    char* output_data = malloc(output_size + 1); // +1 para NULL
+    outputTexto = fopen(nombreArchivoSalida, "w");
+    if (outputTexto == NULL) {
+      printf("No se pudo abrir el archivo.\n");
+      return 1; 
+    }
+  
+    fputs(cadena, outputTexto);//eof
+    fclose(outputTexto);
+  
+  
+    return 0; 
+  }
+  """
+  inputBinario.bin 
+apertura
+lectura
 
-    // Llamar a la función assembler
-    base64_encode(input_data, file_size, output_data);
 
-    // Escribir resultado
-    FILE* output_file = fopen("outputTexto.txt", "w");
-    fwrite(output_data, 1, output_size, output_file);
-    fclose(output_file);
-
-    // Liberar recursos
-    free(input_data);
-    free(output_data);
-    return 0;
-}
+outputTexto.txt
+escritura 
+cierre
+"""
